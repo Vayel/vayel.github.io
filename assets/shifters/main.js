8f7fb299d5873fdad688f4bddc143738d442b8ae
Vincent Lefoulon <vincent.lefoulon@free.fr>
2019-07-01 16:56:58 +0200

    Check multiple choice questions

--- assets/shifters/main.js
+++ assets/shifters/main.js
@@ -2,10 +2,7 @@
   Questions.setup().then(() => {
     let cat = Questions.categories()[0];
     Questions.listFromCategory(cat).then((questions) => {
-      const quiz = Quiz(
-        document.getElementById("quiz"),
-        questions
-      );
+      const quiz = Quiz(document.getElementById("quiz"), questions);
     });
   });
 })();
--- assets/shifters/quiz.js
+++ assets/shifters/quiz.js
@@ -4,18 +4,40 @@ const Quiz = function(wrapper, questions) {
 
     return {
       check: () => {
-        throw "Not implemented";
+        let selected = new Set();
+        for (let option of html.select.options) {
+          if (option.selected) {
+            selected.add(parseInt(option.value));
+          }
+        }
+        return new Immutable.Set(selected).equals(
+          new Immutable.Set(question.answer)
+        );
       },
       render: () => {
         html.wrapper = document.createElement("div");    
         html.title = document.createElement("p");
         html.title.innerText = question.text;
         html.wrapper.appendChild(html.title);
-        // TODO
+
+        html.select = document.createElement("select");
+        html.select.setAttribute("multiple", true);
+
+        for (let i in question.choices) {
+          let option = document.createElement("option");
+          option.value = i;
+          option.innerText = question.choices[i];
+          html.select.appendChild(option);
+        }
+
+        html.wrapper.appendChild(html.select);
         parent.appendChild(html.wrapper);
       },
     };
   };
+
+  let self = {};
+  self.check = () => questions.map(q => q.check());
   
   questions = questions.map(json => {
     switch(json.type) {
@@ -26,10 +48,13 @@ const Quiz = function(wrapper, questions) {
     }
   });
   questions.map(q => q.render());
+  
+  let checkBtn = document.createElement("button");
+  checkBtn.innerText = "Check";
+  checkBtn.addEventListener("click", (e) => {
+    console.log(self.check())
+  });
+  wrapper.appendChild(checkBtn);
 
-  return {
-    check: function() {
-      throw "Not implemented"; 
-    },
-  };
+  return self;
 };
