f2393f7c4b1394d8783caa21e1da0f9de67cb785
Vincent Lefoulon <vincent.lefoulon@free.fr>
2019-07-18 11:57:57 +0200

    Remove previous layer

--- assets/water_scarcity/main.js
+++ assets/water_scarcity/main.js
@@ -1,3 +1,3 @@
 (function() {
-  SSWI("sswi_map", "sswi_horizon", "sswi_season");
+  SSWI("sswi_map", "sswi_horizon", "sswi_season", "spinner");
 })();
--- assets/water_scarcity/sswi.js
+++ assets/water_scarcity/sswi.js
@@ -1,10 +1,11 @@
-const SSWI = (mapId, horizonSelectId, seasonSelectId, rootUrl = "/assets/water_scarcity/data/sswi/") => {
+const SSWI = (mapId, horizonSelectId, seasonSelectId, spinnerId, rootUrl = "/assets/water_scarcity/data/sswi/") => {
   const horizonSelect = document.getElementById(horizonSelectId);
   const seasonSelect = document.getElementById(seasonSelectId);
   const map = L.map(mapId).setView([46.3630104, 2.9846608], 5);
   L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+  let layer;
 
   const getDataFname = () => {
     const h = horizonSelect.options[horizonSelect.selectedIndex].value; 
@@ -21,9 +22,19 @@ const SSWI = (mapId, horizonSelectId, seasonSelectId, rootUrl = "/assets/water_s
     return "black";
   };
 
+  const showSpinner = (show = true) => {
+    const spinner = document.getElementById("spinner");
+    if (show) spinner.style.display = "block";
+    else spinner.style.display = "none";
+  };
+
   const update = () => {
+    showSpinner();
     fetchData(getDataFname()).then((data) => {
-      L.geoJSON(data, {
+      if (layer !== undefined) {
+        map.removeLayer(layer);
+      }
+      layer = L.geoJSON(data, {
         pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
           radius: 3,
           fillColor: sswiToColor(feature.properties.sswi),
@@ -31,7 +42,9 @@ const SSWI = (mapId, horizonSelectId, seasonSelectId, rootUrl = "/assets/water_s
           opacity: 1,
           fillOpacity: 0.8
         })
-      }).addTo(map);
+      });
+      layer.addTo(map);
+      showSpinner(false);
     });
   };
 
