ae43430444bd67baf0fd368eb334fd5e9fab516c
Vincent Lefoulon <vincent.lefoulon@free.fr>
2019-07-27 19:38:11 +0200

    Use horizontal layout

--- assets/water_scarcity/main.js
+++ assets/water_scarcity/main.js
@@ -1,10 +1,10 @@
 (function() {
   SSWI(
-    "sswi_map",
-    "sswi_horizon",
-    "sswi_season",
-    "sswi_colorscale",
+    "map",
+    "horizon",
+    "season",
+    "colorscale",
     ["transparent", "#e08f17", "#a9381c", "#000000"],
-    "sswi_spinner"
+    "spinner"
   );
 })();
